4a112433106b427bdaaf52239c5fe09b9191ad3a
d.chakraborty1612 <d.chakraborty1612@gmail.com>
2022-07-16 15:06:01 +0530

    review

---
+++ Calculator/CalculatorTest/MultiplyTest.cs
@@ -0,0 +1,21 @@
+﻿using Calculator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class MultiplyTest
+    {
+        [Test]
+        public void TestOperate()
+        {
+            IOperate operate = new Multiply();
+            Assert.AreEqual(operate.PerformOperation(3, 6), 18);
+        }
+    }
+}
